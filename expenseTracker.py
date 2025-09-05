fe6582edf5a0cd8742ed7a4c7fc930f500f76362
Luis Berdecia <67439051+BerdeciaLuis@users.noreply.github.com>
2025-09-05 12:07:26 -0400

    Update expenseTracker.py
    Updated the expenseTracker file to be more cleaner

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,92 +1,13 @@
 #Budget/Expense Tracker 
-categories = []
 
-class Category:
+#This 'Application' is being use to learn and allow me to feel more comfortable using Python.
+#Might have a readMe.txt to write my thought process and changes that I might add to the code.
 
-    def __init__(self, name, budget):
-        self.name = name
-        self.budget = budget
-        self.expenses = []
+#This will be the MAIN class for the 'application'
 
-    
-    def remaining_budget(self):
-        return self.budget - self.total_expenses()
-    
-    def add_expense(self, amount, description=""):
-        self.expenses.append((amount, description))
+from budgetManager import budgetManager                            
 
-    def total_expenses(self):
-        return sum(amount for amount, _ in self.expenses)
-    
-    def __str__(self):
-        return f"Category: {self.name}, Budget: {self.budget}, Spent: {self.total_expenses()}, Remaining: {self.remaining_budget()}"
-
-
-
-def Setup_budget():
-    
-    totalRemainingBudget = 0
-    budgetForWeek = 0.00
-
-    print("\n\nWhich type of weekly budget would you like to setup: ")
-    print("1 = Detailed Weekly Budget")
-    print("2 = Simple Weekly Budget")
-
-    answer = input("\n\nPlease select one of the options above (1 - 2): ")
-
-    if answer == "1":       
-        
-        name = input("Enter the category name: ")
-        budget = float(input("Enter budget for this category: "))
-        budgetForWeek += budget
-        print(budgetForWeek)
-        new_category = Category(name, budget)
-        categories.append(new_category)
-
-        while True:
-
-            cont = input("\n\nWould you like to continue to enter categories? (Y/N): ")
-
-            if cont == "N" or cont == "n":
-                break
-
-            elif cont == "Y" or cont == "y" :
-                
-                name = input("Enter the category name: ")
-                budget = float(input("Enter budget for this category: "))
-                budgetForWeek += budget
-                print(budgetForWeek)
-                new_category = Category(name, budget)
-                categories.append(new_category)     
-            
-            else:  
-                print("\n\nInvalid Input. Please try again!")
-
-
-        print("\n\n")
-        print("\nExpense Tracker For Week of \n")
-        print(f"{'Category':<15}{'Budget':<10}{'Spent':<10}{'Remaining':<10}")
-        print("-" * 45)
-
-        for category in categories:
-            print(f"{category.name:<15}{category.budget:<10}{category.total_expenses():<10}{category.remaining_budget():<10}")
-            totalRemainingBudget += category.remaining_budget()
-
-        print("-" * 45)
-        print(f"{'TOTAL':<15}{budgetForWeek:<10}{budgetForWeek-totalRemainingBudget:<10}{totalRemainingBudget:<10}")
-
-       
-  
-    elif answer == "2":
-
-        budget = float(input("Please enter the amount you would like to setup for this week's budget: "))
-        print("The Budget you have allocated for this week is: ", budget)
-
-    else: 
-
-        print("\n\nInvalid Input. Please try again!")
-
-
+manager = budgetManager()
 
 while True:
 
@@ -97,26 +18,19 @@ while True:
     print("4 = Provide Report")
     print("5 = EXIT ")
 
-    choice = input("\n\nPlease select one of the options above (1 - 4): ")
+    choice = input("\n\nPlease select one of the options above (1 - 5): ")
 
     if choice == "1":
 
-        Setup_budget()
+        manager.Setup_budget()
 
     elif choice == "2":
 
-        print()
+        manager.enterExpense()
     
     elif choice == "3":
 
-        if not categories:
-            print("\nNo budget had been set yet.")
-            continue
-
-        
-
-        print()
-
+        manager.viewCurentBudget()
 
     elif choice == "4":
 
@@ -130,3 +44,5 @@ while True:
     else:
 
         print("\n\nInvalid Input. Please try again!")
+
+    
